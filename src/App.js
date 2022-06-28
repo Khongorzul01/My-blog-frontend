748f301ccfe8a6fbd389c66bf11ce49ecd9d6478
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-06-28 19:55:21 +0800

    sdfg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Blog from "../src/component/Blog";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return <Blog />;
 }
--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,22 +1,76 @@
 import React, { useEffect, useState } from "react";
+import { Button, Modal, InputGroup, Form } from "react-bootstrap";
 import "../../src/App.css";
 import { useBlog } from "../context/BlogContext";
-import BlogModal from "../component/modal";
 
 export default function Blog(props) {
   const [blog, setBlog] = useBlog([]);
 
-  const handleShow = () => {
-    setShow(true);
-  };
   const [show, setShow] = useState(false);
+
   const handleClose = () => setShow(false);
-  console.log(blog);
+  const handleShow = () => setShow(true);
+  const createPost = async (e) => {
+    e.preventDefault();
+    return await fetch("http://localhost:5000/api/blog", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // "postdate":"2020/01/12",
+        topic: `${e.target[0].value}`,
+        text: `${e.target[1].value}`,
+        // "image": `${e.target[0].value}`
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message == "success") {
+          handleClose();
+        }
+      });
+  };
+
   return (
     <div>
       <div className="modalSee">
         {show == true ? (
-          <BlogModal show={show} handleClose={handleClose} data={blog} />
+          <div>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add post</Modal.Title>
+              </Modal.Header>
+              <form action="" onSubmit={createPost}>
+                <Modal.Body>
+                  <div className="titles">
+                    <Form.Control
+                      placeholder="Title"
+                      aria-label="Title"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+
+                  <div>
+                    <Form.Control
+                      className="desc"
+                      placeholder="Description"
+                      aria-label="Description"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                </Modal.Body>
+                <Modal.Footer className="buttons">
+                  <Button variant="secondary" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                  <Button variant="primary" type="submit">
+                    + Create Post
+                  </Button>
+                </Modal.Footer>
+              </form>
+            </Modal>
+          </div>
         ) : null}
       </div>
       <div className="blogs">
